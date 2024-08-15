b1cb9c48244b873079b22727fa00b26c36ee07dd
denglibing5 <denglibing3@jd.com>
2024-08-15 16:22:52 +0800

    继续解决编译、运行失败的问题。

--- cocoapods-imy-bin/lib/cocoapods-imy-bin/native/pod_source_installer.rb
+++ cocoapods-imy-bin/lib/cocoapods-imy-bin/native/pod_source_installer.rb
@@ -10,13 +10,20 @@ module Pod
     class PodSourceDownloader
       attr_accessor :installation_options
 
-      alias old_verify_source_is_secure verify_source_is_secure
-      def verify_source_is_secure(root_spec)
-        # http source 默认不警告
-        if installation_options.warn_for_unsecure_source?
-          old_verify_source_is_secure(root_spec)
-        end
-      end
+      # alias old_verify_source_is_secure verify_source_is_secure
+      # def verify_source_is_secure(root_spec)
+      #   # http source 默认不警告
+      #   if installation_options.warn_for_unsecure_source?
+      #     old_verify_source_is_secure(root_spec)
+      #   end
+      # end
+    end
+  end
+
+  class Installer
+    # 新增  PodSourceInstaller 的 installation_options 扩展方法
+    class PodSourceInstaller
+      attr_accessor :installation_options
     end
   end
 end
